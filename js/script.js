c890be048d164f6bd3ae2d39cdeb165c50e78b51
afablelorenz <afablelorenz@gmail.com>
2024-05-22 21:31:05 +0200

    created functions for printing object info in dom

--- js/script.js
+++ js/script.js
@@ -1,37 +1,74 @@
+const teamGrid = document.querySelector('section#teamGrid');
 let team = [
-    member1 = {
+    {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
         foto: 'wayne-barnett-founder-ceo.jpg'
     },
 
-    member2 = {
+    {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
         foto: 'angela-caroll-chief-editor.jpg'
     },
 
-    member3 = {
+    {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
         foto: 'walter-gordon-office-manager.jpg'
     },
 
-    member4 = {
+    {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
         foto: 'angela-lopez-social-media-manager.jpg'
     },
 
-    member5 = {
+    {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
         foto: 'scott-estrada-developer.jpg'
     },
 
-    member6 = {
+    {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         foto: 'barbara-ramos-graphic-designer.jpg'
     },
 ];
+
+
+for(let i = 0; i < team.length; i++){
+    const teamArticle = document.createElement('article');
+    teamArticle.classList.add('teamArticle');
+    teamGrid.appendChild(teamArticle);
+}
+
+for(let i=0; i<team.length; i++){
+    teamNameAppend(team[i]);
+    teamRoleAppend(team[i]);
+    teamImageAppend(team[i]);
+}
+
+
+function teamNameAppend(array){
+    const teamArticle = document.querySelector('article.teamArticle');
+    let teamArticleAdd=document.createTextNode(array.nome);
+    teamArticle.appendChild(teamArticleAdd);
+}
+
+function teamRoleAppend(array){
+    const teamArticle = document.querySelector('article.teamArticle');
+    let teamArticleAdd=document.createTextNode(array.ruolo);
+    teamArticle.appendChild(teamArticleAdd);
+}
+
+function teamImageAppend(array){
+    const teamArticle = document.querySelector('article.teamArticle');
+    let teamArticleAdd=document.createTextNode(array.foto);
+    teamArticle.appendChild(teamArticleAdd);
+}
+
+
+
+
