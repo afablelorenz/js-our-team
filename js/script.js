b7646c4b3d4676bcb856b6099a1110a08f40befb
afablelorenz <afablelorenz@gmail.com>
2024-05-22 19:08:54 +0200

    created array of objects

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let team = [
+    member1 = {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    member2 = {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+
+    member3 = {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+
+    member4 = {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    member5 = {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+
+    member6 = {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    },
+];
